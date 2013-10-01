a54fe29617fffab0622cd779e5eb26991e747269
Jay Karim and Jackson Popkin <jfk303@nyu.edu>
2013-10-01 14:28:55 -0400

    Implements back to index button on show page

--- app/assets/javascripts/views/post_show_view.js
+++ app/assets/javascripts/views/post_show_view.js
@@ -5,8 +5,18 @@ JNL.Views.PostShowView = Backbone.View.extend({
 
     var $header = $("<h1>" + that.post.get("title") + "</h1>");
     var $body = $("<p>" + that.post.get("body") + "</p>");
+    var $back = $("<button class='back'>Back to Index</button>");
     that.$el.append($header);
     that.$el.append($body);
+    that.$el.append($back);
     return that;
+  },
+
+  events: {
+    "click button.back": "backToIndex"
+  },
+
+  backToIndex: function(event) {
+    Backbone.history.navigate("#/");
   }
 });
